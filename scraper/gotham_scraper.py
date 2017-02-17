4be5938c01162c64410cc8f225dcd0223e02ea44
salimmj <msm2243@columbia.edu>
2017-02-17 18:33:21 -0500

    localhost

---
+++ scraper/gotham_scraper.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import urllib.request, urllib.parse, urllib.error
+
+""" Given a URL, returns a BeautifulSoup object for that page
+"""
+
+def get_soup(url):
+    raw_url = urllib.request.urlopen(url)
+    raw_page = raw_url.read()
+    soup = BeautifulSoup(raw_page, "html.parser")
+    return soup
+
+
+def scrape_new_stories_URLs():
+    soup = get_soup("http://www.columbia.edu/~msm2243/")
+
+#    stories = soup.find_all("li", class_="latestnews-item catid-126 featured")
+
+#   print(str(stories))
+    print(str(soup))
+
+scrape_new_stories_URLs()
