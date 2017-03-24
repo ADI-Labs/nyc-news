d6d832c8244ce950499b293848d64e1cc5a53599
Sejal Jain <sejaljain@dyn-160-39-148-181.dyn.columbia.edu>
2017-03-24 16:26:32 -0400

    fixed officials panel

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, jsonify
 import requests, json
 from firebase import firebase
 import itertools
+
 
 app = Flask(__name__)
 firebase = firebase.FirebaseApplication("https://labs-2c94e.firebaseio.com", None)
@@ -13,15 +15,15 @@ firebase = firebase.FirebaseApplication("https://labs-2c94e.firebaseio.com", Non
 @app.route("/", methods = ["GET", "POST"])
 def homepage():
     if request.method == "GET":
-    	url = "https://www.googleapis.com/civicinfo/v2/representatives?address=10027&key=AIzaSyAalDN2dXO26te2Soy9gAsOU_wvSlYghVg"
-    	res = requests.get(url).json()
+    	url = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + "New York City" + "&key=AIzaSyAalDN2dXO26te2Soy9gAsOU_wvSlYghVg"
+    	res = json.loads(requests.get(url).text)
     	articles = results("default")
-    	return render_template("index.html", articles = articles, reps = res)
+    	return render_template("index.html", articles = articles, res = res)
     else:
-    	url = "https://www.googleapis.com/civicinfo/v2/representatives?address=10027&key=AIzaSyAalDN2dXO26te2Soy9gAsOU_wvSlYghVg"
-    	res = requests.get(url).json()
+    	url = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + request.form["location"] + "&key=AIzaSyAalDN2dXO26te2Soy9gAsOU_wvSlYghVg"
+        res = json.loads(requests.get(url).text, encoding='utf-8')
     	articles = results(request.form["location"])
-    	return render_template("index.html", articles = articles, reps = res)
+        return render_template("index.html", articles = articles, res = res)
 
 @app.errorhandler(404)
 def page_not_found(error):
